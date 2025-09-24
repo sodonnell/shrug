8586380c298f1fe2057b8f08875b7f68f33f8a2e
Sean O'Donnell <sean@seanodonnell.com>
2025-09-24 12:20:05 -0500

    Created shrug.c, 'cuz "Hello World" is boring! ¯\_(ツ)_/¯
    'cuz "Hello World" is boring! ¯\_(ツ)_/¯

---
+++ shrug.c
@@ -0,0 +1,14 @@
+/** 
+*
+* 'cuz "Hello World" is boring! ¯\_(ツ)_/¯
+*
+* Author: Sean O'Donnell <sean@seanodonnell.com>
+*
+*/
+#include <stdio.h>
+
+int main() {
+    char emoji[] = "¯\\_(ツ)_/¯";
+    printf("%s\n", emoji);
+    return 0;
+}
